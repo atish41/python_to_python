120eaa1fb9933e5440d7ee19b33c5f78b46bf536
atish41 <123723759+atish41@users.noreply.github.com>
2023-12-09 22:58:03 +0530

    Add files via upload

---
+++ ins.py
@@ -0,0 +1,11 @@
+#isinstance(p1,p2)
+
+def check(obj):
+	if isinstance(obj,str):
+		print("this is string data type")
+	elif isinstance(obj,int):
+		print("this is integer data type")
+
+value=654
+check(value)
+	
